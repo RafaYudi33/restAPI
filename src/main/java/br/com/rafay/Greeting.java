52fd413e4297b56038a4e247d7b44b388bcdc608
RafaYudi33 <rafael.tanaka@unesp.br>
2023-12-09 11:42:26 -0300

    restCalculator sumMethod

---
+++ src/main/java/br/com/rafay/Greeting.java
@@ -0,0 +1,11 @@
+package br.com.rafay;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Greeting {
+    private final long id; 
+    private final String content; 
+}
